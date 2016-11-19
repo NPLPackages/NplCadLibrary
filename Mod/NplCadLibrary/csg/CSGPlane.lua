b27c7a1e81e32fe5ce19783118a57c6fa53c3cb1
LiXizhi <lxz1982@hotmail.com>
2016-11-20 00:58:28 +0800

    optimized local performance

--- Mod/NplCadLibrary/csg/CSGPlane.lua
+++ Mod/NplCadLibrary/csg/CSGPlane.lua
@@ -1,8 +1,9 @@
 --[[
 Title: CSGPlane
-Author(s): leio
+Author(s): leio, LiXizhi
 Date: 2016/3/29
-Desc: 
+Desc: read-only plane. self.normal uses copy on write policy. 
+
 Represents a plane in 3D space.
 -------------------------------------------------------
 NPL.load("(gl)Mod/NplCadLibrary/csg/CSGPlane.lua");
@@ -14,99 +15,115 @@ NPL.load("(gl)Mod/NplCadLibrary/csg/CSGPolygon.lua");
 local CSGVector = commonlib.gettable("Mod.NplCadLibrary.csg.CSGVector");
 local CSGPolygon = commonlib.gettable("Mod.NplCadLibrary.csg.CSGPolygon");
 local CSGPlane = commonlib.inherit(nil, commonlib.gettable("Mod.NplCadLibrary.csg.CSGPlane"));
+
+local bor = mathlib.bit.bor;
+
 --`CSG.Plane.EPSILON` is the tolerance used by `splitPolygon()` to decide if a
 -- point is on the plane.
 CSGPlane.EPSILON = 0.00001;
+
+
 function CSGPlane:ctor()
 end
+
 function CSGPlane:init(normal, w)
+	-- normal is read-only
 	self.normal = normal;
 	self.w = w;
 	return self;
 end
 function CSGPlane.fromPoints(a, b, c)
-	local n = b:minus(a):cross(c:minus(a)):unit();
+	local n = b:minus(a):crossInplace(c:clone_from_pool():minusInplace(a)):unitInplace();
 	local plane = CSGPlane:new():init(n,n:dot(a));
 	return plane;
 end
 function CSGPlane:clone()
-	local plane = CSGPlane:new():init(self.normal:clone(),self.w);
+	local plane = CSGPlane:new():init(self.normal,self.w);
 	return plane;
 end
 function CSGPlane:flip()
 	self.normal = self.normal:negated();
 	self.w = -self.w;
 end
+
+local types = {};
+
+local COPLANAR = 0;
+local FRONT = 1;
+local BACK = 2;
+local SPANNING = 3;
+
 -- Split `polygon` by this plane if needed, then put the polygon or polygon
 -- fragments in the appropriate lists. Coplanar polygons go into either
 -- `coplanarFront` or `coplanarBack` depending on their orientation with
 -- respect to this plane. Polygons in front or in back of this plane go into
 -- either `front` or `back`.
 function CSGPlane:splitPolygon(polygon, coplanarFront, coplanarBack, front, back)
-	local COPLANAR = 0;
-    local FRONT = 1;
-    local BACK = 2;
-    local SPANNING = 3;
-
     --Classify each point as well as the entire polygon into one of the above four classes.
     local polygonType = 0;
-    local types = {};
-	for k,v in ipairs(polygon.vertices) do
+    
+	local EPSILON = CSGPlane.EPSILON;
+	for i,v in ipairs(polygon.vertices) do
 		local t = self.normal:dot(v.pos) - self.w;
 		local type;
-		if(t < -CSGPlane.EPSILON)then
+		if(t < -EPSILON)then
 			type = BACK;
-		elseif(t > CSGPlane.EPSILON)then
+		elseif(t > EPSILON)then
 			type = FRONT;
 		else
 			type = COPLANAR;
 		end
-		polygonType = mathlib.bit.bor(polygonType, type);
-		table.insert(types,type);
+		polygonType = bor(polygonType, type);
+		types[i] = type;
 	end
 	if(polygonType == COPLANAR)then
 		if(self.normal:dot(polygon.plane.normal) > 0)then
-			table.insert(coplanarFront,polygon);
+			coplanarFront[#coplanarFront+1] = polygon;
 		else
-			table.insert(coplanarBack,polygon);
+			coplanarBack[#coplanarBack+1] = polygon;
 		end
 	elseif(polygonType == FRONT)then
-		table.insert(front,polygon);
+		front[#front+1] = polygon;
 	elseif(polygonType == BACK)then
-		table.insert(back,polygon);
+		back[#back+1] = polygon;
 	elseif(polygonType == SPANNING)then
+		local backCount, frontCount = 0, 0;
 		local f = {};
 		local b = {};
 		local size = #polygon.vertices;
-		for k = 0,size-1 do
-			local i = k + 1;
-			local j = math.mod(k+1,size) + 1;
+		for i = 1, size do
+			local j = (i % size) + 1;
 			local ti = types[i];
 			local tj = types[j];
 			local vi = polygon.vertices[i]
 			local vj = polygon.vertices[j];
 			if(ti ~= BACK)then
-				table.insert(f,vi);
+				frontCount = frontCount + 1;
+				f[frontCount] = vi;
 			end
 			if(ti ~= FRONT)then
 				if(ti ~= BACK)then
-					table.insert(b,vi:clone());
+					backCount = backCount + 1;
+					b[backCount] = vi:clone();
 				else
-					table.insert(b,vi);
+					backCount = backCount + 1;
+					b[backCount] = vi;
 				end
 			end
-			if(mathlib.bit.bor(ti, tj) == SPANNING)then
+			if(bor(ti, tj) == SPANNING)then
 				local t = (self.w - self.normal:dot(vi.pos)) / self.normal:dot(vj.pos:minus(vi.pos));
 				local v = vi:interpolate(vj, t);
-				table.insert(f,v);
-				table.insert(b,v:clone());
+				frontCount = frontCount + 1;
+				f[frontCount] = v;
+				backCount = backCount + 1;
+				b[backCount] = v:clone();
 			end
 		end
-		if((#f) >= 3)then
-			table.insert(front,CSGPolygon:new():init(f,polygon.shared));
+		if(frontCount >= 3)then
+			front[#front+1] = CSGPolygon:new():init(f,polygon.shared);
 		end
-		if((#b) >= 3)then
-			table.insert(back,CSGPolygon:new():init(b,polygon.shared));
+		if(backCount >= 3)then
+			back[#back+1] = CSGPolygon:new():init(b,polygon.shared);
 		end
 	end
 end
--- Mod/NplCadLibrary/csg/CSGVector.lua
+++ Mod/NplCadLibrary/csg/CSGVector.lua
@@ -1,6 +1,6 @@
 --[[
 Title: CSGVector
-Author(s): leio
+Author(s): leio, LiXizhi
 Date: 2016/3/29
 Desc: 
 Represents a plane in 3D space.
@@ -16,11 +16,66 @@ echo(v1:minus(v2));
 ]]
 local math_abs = math.abs;
 local CSGVector = commonlib.inherit(nil, commonlib.gettable("Mod.NplCadLibrary.csg.CSGVector"));
+
+--------------------
+-- private vector pool
+--------------------
+local VectorPool = {};
+VectorPool.__index = VectorPool;
+
+-- we will automatically reuse from beginning when reaching this value. 
+VectorPool.maxPoolSize = 300;
+
+function VectorPool:new()
+	local o = {};
+	-- Number of times this Pool has been cleaned
+	o.numCleans = 0;
+	-- List of vector stored in this Pool
+	o.listVector3D = commonlib.vector:new();
+	-- Next index to use when adding a Pool Entry.
+	o.nextPoolIndex = 1;
+	-- Largest index reached by this Pool since last CleanPool operation. 
+	o.maxPoolIndex = 0;
+	-- Largest index reached by this Pool since last Shrink operation. 
+	o.maxPoolIndexFromLastShrink = 0;
+	o.maxPoolSize = VectorPool.maxPoolSize;
+	setmetatable(o, self);
+	return o;
+end
+
+-- Creates a new Vector, or reuses one that's no longer in use. 
+-- @param x,y,z:
+-- returns from this function should only be used for one frame or tick, as after that they will be reused.
+function VectorPool:GetVector(x,y,z)
+    local vec3d;
+
+    if (self.nextPoolIndex > self.listVector3D:size()) then
+		vec3d = CSGVector:new():set(x,y,z);
+        self.listVector3D:add(vec3d);
+    else
+        vec3d = self.listVector3D:get(self.nextPoolIndex);
+		vec3d:set(x,y,z);
+    end
+
+    self.nextPoolIndex = self.nextPoolIndex + 1;
+	if(self.nextPoolIndex > self.maxPoolSize) then
+		self.maxPoolIndex = self.maxPoolSize;
+		self.nextPoolIndex = 1;
+	end
+    return vec3d;
+end
+
+local default_pool = VectorPool:new();
+
+------------------------------------------
+-- CSG Vector
+------------------------------------------
 function CSGVector:ctor()
 	self.x = 0;
 	self.y = 0;
 	self.z = 0;
 end
+
 function CSGVector:init(x,y,z)
 	if y == nil then
 		if(type(x) == "table")then
@@ -39,29 +94,64 @@ function CSGVector:init(x,y,z)
     end
 	return self;
 end
+
+CSGVector.set = CSGVector.init;
+
 function CSGVector:clone()
 	return CSGVector:new():init(self.x,self.y,self.z);
 end
+
+function CSGVector:new_from_pool(x,y,z)
+	return default_pool:GetVector(x,y,z);
+end
+
+function CSGVector:clone_from_pool()
+	return default_pool:GetVector(self.x,self.y,self.z);
+end
+
 function CSGVector:negated()
 	return CSGVector:new():init(-self.x,-self.y,-self.z);
+end
+function CSGVector:negatedInplace()
+	self.x, self.y, self.z = -self.x,-self.y,-self.z;
+	return self;
 end
 function CSGVector:plus(a)
 	return CSGVector:new():init(self.x + a.x,self.y + a.y,self.z + a.z);
 end
+function CSGVector:plusInplace(a)
+	self.x, self.y, self.z = self.x + a.x,self.y + a.y,self.z + a.z;
+	return self;
+end
 function CSGVector:minus(a)
 	return CSGVector:new():init(self.x - a.x,self.y - a.y,self.z - a.z);
+end
+function CSGVector:minusInplace(a)
+	self.x, self.y, self.z = self.x - a.x,self.y - a.y,self.z - a.z;
+	return self;
 end
 function CSGVector:times(a)
 	return CSGVector:new():init(self.x * a,self.y * a,self.z * a);
 end
+function CSGVector:timesInplace(a)
+	self.x, self.y, self.z = self.x * a,self.y * a,self.z * a;
+	return self;
+end
+
 function CSGVector:dividedBy(a)
 	return CSGVector:new():init(self.x / a,self.y / a,self.z / a);
 end
+
+function CSGVector:dividedByInplace(a)
+	self.x, self.y, self.z = self.x / a,self.y / a,self.z / a;
+	return self;
+end
+
 function CSGVector:dot(a)
 	return self.x * a.x + self.y * a.y + self.z * a.z;
 end
 function CSGVector:lerp(a,t)
-	return self:plus(a:minus(self):times(t));
+	return a:minus(self):timesInplace(t):plusInplace(self);
 end
 function CSGVector:length()
 	return math.sqrt(self:dot(self));
@@ -69,12 +159,30 @@ end
 function CSGVector:unit()
 	return self:dividedBy(self:length());
 end
+function CSGVector:unitInplace()
+	return self:dividedByInplace(self:length());
+end
+
 function CSGVector:cross(a)
 	return CSGVector:new():init( 
 		self.y * a.z - self.z * a.y,
 		self.z * a.x - self.x * a.z,
 		self.x * a.y - self.y * a.x);
 end
+
+function CSGVector:crossInplace(a)
+	self.x, self.y, self.z = 
+			self.y * a.z - self.z * a.y,
+			self.z * a.x - self.x * a.z,
+			self.x * a.y - self.y * a.x;
+	return self;
+end
+
 function CSGVector:abs()
 	return CSGVector:new():init(math_abs(self.x),math_abs(self.y),math_abs(self.z));
+end
+
+function CSGVector:absInplace()
+	self.x, self.y, self.z = math_abs(self.x),math_abs(self.y),math_abs(self.z);
+	return self;
 end
